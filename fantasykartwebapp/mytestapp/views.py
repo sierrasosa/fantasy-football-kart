1b3bad9356977209ec9bbf80c3eb89d77d71bf8d
sierrasosa <sierra.n.sosa@gmail.com>
2026-09-16 21:02:43 -0700

    switch to streamlit

--- fantasykartwebapp/mytestapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, HttpResponse
-import requests
-
-# Create your views here.
-def home(request):
-    res = requests.get('https://api.sleeper.app/v1/user/731795370941743104')
-    return HttpResponse(res.json()['display_name'])
